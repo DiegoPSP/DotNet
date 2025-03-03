c8d735effbd45f1d7de52c9aeef8a598430d6224
DiegoPSP <113466295+DiegoPSP@users.noreply.github.com>
2025-03-03 19:13:28 -0300

    Add files via upload

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,34 @@
+﻿using ExemploPOO.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+
+// p1.Nome = "Diego";
+// p1.Idade = 22;
+
+// p1.Apresentar();
